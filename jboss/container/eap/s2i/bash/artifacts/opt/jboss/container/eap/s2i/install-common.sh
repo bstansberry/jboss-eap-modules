0927988652e528dc8cbb03c4c443a666d55c1e6d
Brian Stansberry <brian.stansberry@redhat.com>
2019-03-12 19:05:33 -0500

    Fetch driver from maven WIP

--- jboss/container/eap/s2i/bash/artifacts/opt/jboss/container/eap/s2i/install-common.sh
+++ jboss/container/eap/s2i/bash/artifacts/opt/jboss/container/eap/s2i/install-common.sh
@@ -45,25 +45,75 @@ function configure_drivers(){
     drivers=
     if [ -n "$DRIVERS" ]; then
       for driver_prefix in $(echo $DRIVERS | sed "s/,/ /g"); do
+	    # If the prefix is one of our standard ones, set up default values
+	    # for things that would otherwise have to be by env vars
+	    case "$db" in
+		    "mysql")
+                # TODO mysql values
+                standard_module=""
+                standard_driver_name=""
+                standard_driver_class=""
+                standard_driver_artifact=""
+		     ;;
+		    "postgresql")
+                # TODO postgres values
+                standard_module=""
+                standard_driver_name=""
+                standard_driver_class=""
+                standard_driver_artifact=""
+		    ;;
+		    "mongodb")
+                # TODO mongodb values
+                standard_module=""
+                standard_driver_name=""
+                standard_driver_class=""
+                standard_driver_artifact=""
+		    ;;
+		    *)
+                standard_module=""
+                standard_driver_name=""
+                standard_driver_class=""
+                standard_driver_artifact=""
+		    ;;
+		esac
+		
         driver_module=$(find_env "${driver_prefix}_DRIVER_MODULE")
         if [ -z "$driver_module" ]; then
-          echo "Warning - ${driver_prefix}_DRIVER_MODULE is missing from driver configuration. Driver will not be configured"
-          continue
+          if [ -n "$standard_module" ]; then
+            driver_module=$(standard_module)
+          else
+            echo "Warning - ${driver_prefix}_DRIVER_MODULE is missing from driver configuration. Driver will not be configured"
+            continue
+          fi
         fi
       
         driver_name=$(find_env "${driver_prefix}_DRIVER_NAME")
         if [ -z "$driver_name" ]; then
-          echo "Warning - ${driver_prefix}_DRIVER_NAME is missing from driver configuration. Driver will not be configured"
-          continue
+          if [ -n "$standard_driver_name" ]; then
+            driver_name=$(standard_driver_name)
+          else
+            echo "Warning - ${driver_prefix}_DRIVER_NAME is missing from driver configuration. Driver will not be configured"
+            continue
+          fi
         fi
 
         driver_class=$(find_env "${driver_prefix}_DRIVER_CLASS")
         datasource_class=$(find_env "${driver_prefix}_XA_DATASOURCE_CLASS")
         if [ -z "$driver_class" ] && [ -z "$datasource_class" ]; then
-          echo "Warning - ${driver_prefix}_DRIVER_NAME and ${driver_prefix}_XA_DATASOURCE_CLASS is missing from driver configuration. At least one is required. Driver will not be configured"
-          continue
+          if [ -n "$standard_driver_class" ]; then
+            driver_name=$(standard_driver_class)
+          else
+            echo "Warning - ${driver_prefix}_DRIVER_NAME and ${driver_prefix}_XA_DATASOURCE_CLASS is missing from driver configuration. At least one is required. Driver will not be configured"
+            continue
+          fi
         fi
-
+ 
+        driver_artifacts=$(find_env "${driver_prefix}_DRIVER_ARTIFACTS")
+        if [ -n "$driver_artifacts" ] || [ -n "$standard_driver_artifact" ]; then
+          # Fetch the driver artifacts and create a module for them
+          configure_driver_module $driver_module $driver_artifacts $standard_driver_artifact
+        fi
+	
         drivers="${drivers} <driver name=\"$driver_name\" module=\"$driver_module\">"
         if [ -n "$datasource_class" ]; then
           drivers="${drivers}<xa-datasource-class>${datasource_class}</xa-datasource-class>"
@@ -81,5 +131,17 @@ function configure_drivers(){
       fi
     fi
   )
+}
+
+configure_driver_module(){
+  local driver_module="${1}"
+  local driver_artifacts="${2}"
+  local standard_driver_artifact="${3}"
+  # TODO 
+  # See if there's a module dir corresponding to driver_module. If not create.
+  # See if there's a module.xml; if not create a standard one
+  # If we created the module.xml and driver_artifacts is empty, set it to standard_driver_artifact (to fetch the default)
+  # For each element in driver_artifacts, use mvn dependency:get and mvn dependency:copy to fetch the artifact and put it in the module
+  # For each element in driver_artifacts, insert a <resource> entry in the module.xml
 }
 
